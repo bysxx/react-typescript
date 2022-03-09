db544c59c7b5ed4ef88aa6c4ca87a45800f1993c
bysxx <0415white@naver.com>
2022-03-09 20:43:31 +0900

    react+typescript - football components update

--- src/routes/football/Football.tsx
+++ src/routes/football/Football.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = process.env.REACT_APP_FOOTBALL_API_KEY;
+
+interface FootballDataProps {}
+
 function Football() {
+  const [footballData, setFootballData] = useState<Promise<any>>();
+
+  const getFootballData = async (endpoint: string) => {
+    const data = await fetch(`https://v3.football.api-sports.io/${endpoint}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "v3.football.api-sports.io",
+      },
+      redirect: "follow",
+    });
+
+    const json = data.json();
+
+    console.log(json);
+    setFootballData(json);
+  };
+
+  useEffect(() => {
+    getFootballData("teams/seasons?team=33");
+  }, []);
+
   return (
     <div>
-      <div>FOOTBALL SOON</div>
+      <div>{footballData}</div>
     </div>
   );
 }
