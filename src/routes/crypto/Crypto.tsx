37ed9eef852421255f5de4b6a7311fc5f7c93fcc
bysxx <0415white@naver.com>
2022-03-23 22:16:08 +0900

    CLN: Coin Main Page Refactoring

--- src/routes/crypto/Crypto.tsx
+++ src/routes/crypto/Crypto.tsx
@@ -2,34 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './Crypto.css';
 import Coin, { coinState } from './components/Coin';
 import { getRequest } from '../../libs/axiosManager';
+import useSWR from 'swr';
+
+const url = `https://api.coinpaprika.com/v1/ticker`;
 
 function Crypto() {
-  const [cryptoData, setCryptoData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const indexes: number[] = [1, 2, 3, 4, 5, 6];
-
-  const getCoinData = async () => {
-    const data = await getRequest<coinState[]>('https://api.coinpaprika.com/v1/tickers', undefined);
-
-    setLoading(false);
-    setCryptoData(data);
-  };
-
-  useEffect(() => {
-    getCoinData();
-  }, []);
+  const indexes: number[] = [0, 1, 2, 3, 4, 5];
+  const { data } = useSWR([url], () => getRequest<coinState[]>(url, undefined));
 
   return (
     <div className={'cryptoContainer'}>
-      {loading ? (
+      {data ? (
         <div className={'loader'}>
           <span>Loading...</span>
         </div>
       ) : (
         <div className={'coins'}>
           {indexes.map((i) => (
-            <Coin key={i} index={i} coinsData={cryptoData} />
+            <Coin key={i} index={i} coinsData={data} />
           ))}
         </div>
       )}
